4030f8e890a800cffd41c32edac3486862e7ac32
TonmoyX <142309932+TonmoyX@users.noreply.github.com>
2025-02-04 00:57:50 +0600

    Add files via upload

---
+++ A+B_Again.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t, n;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int l = n/10;
+        int m = n%10;
+        int sum = l+m;
+        cout<<sum<<endl;
+
+    }
+}
